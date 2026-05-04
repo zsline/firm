fb762600e69b3e5ae6a477d1fdbb1fe1d8bb90c3
zsline <sergei.dereka@gmail.com>
2026-05-04 11:36:30 +0300

    about main

--- assets/js/particals/tabs.js
+++ assets/js/particals/tabs.js
@@ -1,18 +1,13 @@
 export function tabs(){
-    const tabs = document.querySelectorAll('.sidebar__tabs--item');
-    const contents = document.querySelectorAll('.page-tab__content');
+    const tabs = document.querySelectorAll('.page__item');
     tabs.forEach(tab => {
         tab.addEventListener('click', (e) => {
-            tabs.forEach(el => {
-                el.classList.remove('active');
-            });
-            e.target.classList.add('active');
-            contents.forEach(c =>{
-                c.classList.remove('active');
-                if(c.dataset.content == e.target.dataset.tab){
-                    c.classList.add('active')
-                }
-            })
+            if(e.target.className == 'page__item--tab'){
+                tabs.forEach(el => {
+                    el.classList.remove('active');
+                });
+                e.target.parentElement.classList.add('active');
+            }
         })
     })
 }
